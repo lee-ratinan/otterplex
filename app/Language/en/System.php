1b97678a2f4b8fe257564bea0134cadd01ee2be7
Ratinan Lee <lee.ratinan@gmail.com>
2025-11-30 13:04:03 +0800

    forgot password

--- app/Language/en/System.php
+++ app/Language/en/System.php
@@ -27,7 +27,7 @@ return [
             'full-name'             => 'Full name',
             'full-name-empty-error' => 'Please enter your full name.',
             'username'              => 'Username (email address)',
-            'username-empty-error'  => 'Please enter your username.',
+            'username-empty-error'  => 'Please enter your username (email address).',
             'password'              => 'Password',
             'password-empty-error'  => 'Please enter your password.',
             'plan'                  => 'Select your plan',
@@ -42,5 +42,12 @@ return [
             'terms-error'           => 'You must agree before creating your account.'
         ],
         'already-have-account' => 'Already have an account?',
+    ],
+    'forgot-password' => [
+        'instruction'    => 'Please enter your email address and follow the instructions on how to reset your password.',
+        'email-address'  => 'Email address',
+        'submit'         => 'Reset Password',
+        'submit-success' => 'Your reset password request has been received. Please check your email and click the link to reset your password within 5 minutes.',
+        'submit-error'   => 'An error has occurred, please try again later.'
     ]
 ];
--- app/Views/home/forgot_password.php
+++ app/Views/home/forgot_password.php
@@ -1,4 +1,26 @@
 <?php $this->extend('home/_layout'); ?>
 <?= $this->section('content') ?>
-forget password
+    <div class="pt-4 pb-2">
+        <h5 class="card-title text-center pb-0 fs-4"><?= lang('System.pages.forgot-password') ?></h5>
+        <p class="text-center small"><?= lang('System.forgot-password.instruction') ?></p>
+    </div>
+    <form class="row g-3 needs-validation" novalidate="">
+        <div class="col-12">
+            <label for="username" class="form-label"><?= lang('System.login.fields.username') ?></label>
+            <div class="input-group has-validation">
+                <span class="input-group-text" id="inputGroupPrepend1"><i class="bi bi-envelope"></i></span>
+                <input type="email" name="username" class="form-control" id="username" required="">
+                <div class="invalid-feedback"><?= lang('System.login.fields.username-empty-error') ?></div>
+            </div>
+        </div>
+        <div class="col-12">
+            <button class="btn btn-primary w-100" type="submit"><?= lang('System.forgot-password.submit') ?></button>
+        </div>
+        <div class="col-12">
+            <div class="btn-group btn-group-sm w-100">
+                <a class="btn btn-outline-primary" href="<?= getenv('main_site') ?>"><i class="bi bi-chevron-left"></i> <?= lang('System.go-to-main-site') ?></a>
+                <a class="btn btn-outline-primary" href="<?= base_url() ?>"><?= lang('System.pages.login') ?> <i class="bi bi-chevron-right"></i></a>
+            </div>
+        </div>
+    </form>
 <?php $this->endSection() ?>
