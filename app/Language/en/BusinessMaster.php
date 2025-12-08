500c4c375a203b2b5c7bb9eda17412fced888449
Ratinan Lee <lee.ratinan@gmail.com>
2025-12-09 00:28:05 +0800

    add fields to business

--- app/Language/en/BusinessMaster.php
+++ app/Language/en/BusinessMaster.php
@@ -1,20 +1,23 @@
 <?php
 return [
     'field' => [
-        'id'                   => 'ID',
-        'business_type_id'     => 'Business Type',
-        'business_name'        => 'Business Name',
-        'business_slug'        => 'Business Slug',
-        'business_local_names' => 'Business Local Names',
-        'country_code'         => 'Country',
-        'currency_code'        => 'Currency',
-        'tax_percentage'       => 'Tax Percentage',
-        'tax_inclusive'        => 'Tax Inclusive',
-        'contract_expiry'      => 'Contract Expiry Date',
-        'business_status'      => 'Business Status', // Not a real field
+        'id'                    => 'ID',
+        'business_type_id'      => 'Business Type',
+        'business_name'         => 'Business Name',
+        'business_slug'         => 'Business Slug',
+        'business_local_names'  => 'Business Local Names',
+        'country_code'          => 'Country',
+        'currency_code'         => 'Currency',
+        'tax_percentage'        => 'Tax Percentage',
+        'tax_inclusive'         => 'Tax Inclusive',
+        'mart_primary_color'    => 'Martketplace’s Primary Color',
+        'mart_text_color'       => 'Martketplace’s Text Color',
+        'mart_background_color' => 'Martketplace’s Background Color',
+        'contract_expiry'       => 'Contract Expiry Date',
+        'business_status'       => 'Business Status', // Not a real field
     ],
     'enum'  => [
-        'tax_inclusive' => [
+        'tax_inclusive'   => [
             'I' => 'Inclusive',
             'E' => 'Exclusive',
         ],
--- app/Language/th/BusinessMaster.php
+++ app/Language/th/BusinessMaster.php
@@ -1,20 +1,23 @@
 <?php
 return [
     'field' => [
-        'id'                   => 'ไอดี',
-        'business_type_id'     => 'ประเภทธุรกิจ',
-        'business_name'        => 'ชื่อธุรกิจ',
-        'business_slug'        => 'รหัสธุรกิจ',
-        'business_local_names' => 'ชื่อธุรกิจในภาษาอื่น',
-        'country_code'         => 'ประเทศ',
-        'currency_code'        => 'สกุลเงิน',
-        'tax_percentage'       => 'อัตราภาษี',
-        'tax_inclusive'        => 'การคิดภาษี',
-        'contract_expiry'      => 'วันหมดอายุสัญญา',
-        'business_status'      => 'สถานะธุรกิจ', // Not a real field
+        'id'                    => 'ไอดี',
+        'business_type_id'      => 'ประเภทธุรกิจ',
+        'business_name'         => 'ชื่อธุรกิจ',
+        'business_slug'         => 'รหัสธุรกิจ',
+        'business_local_names'  => 'ชื่อธุรกิจในภาษาอื่น',
+        'country_code'          => 'ประเทศ',
+        'currency_code'         => 'สกุลเงิน',
+        'tax_percentage'        => 'อัตราภาษี',
+        'tax_inclusive'         => 'การคิดภาษี',
+        'mart_primary_color'    => 'สีหลักของมาร์เก็ตเพลส',
+        'mart_text_color'       => 'สีข้อความของมาร์เก็ตเพลส',
+        'mart_background_color' => 'สีพื้นหลังของมาร์เก็ตเพลส',
+        'contract_expiry'       => 'วันหมดอายุสัญญา',
+        'business_status'       => 'สถานะธุรกิจ', // Not a real field
     ],
     'enum'  => [
-        'tax_inclusive' => [
+        'tax_inclusive'   => [
             'I' => 'รวมในราคา',
             'E' => 'ไม่รวมในราคา',
         ],
