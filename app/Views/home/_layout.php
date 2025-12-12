5f06947cea8294559f241cb5a3017cc3993bfdd5
Ratinan Lee <lee.ratinan@gmail.com>
2025-12-12 21:26:58 +0800

    layout

--- app/Views/home/_layout.php
+++ app/Views/home/_layout.php
@@ -52,18 +52,15 @@
                                 <?= $this->renderSection('content') ?>
                                 <hr class="mt-0" />
                                 <div class="row">
-                                    <div class="col-6">
+                                    <div class="col">
                                         <i class="bi bi-translate" title="<?= lang('System.change-language') ?>"></i>
                                         <?php if ('en' == $lang) : ?>English<?php else: ?><a href="?hl=en">English</a><?php endif; ?> |
                                         <?php if ('th' == $lang) : ?>ภาษาไทย<?php else: ?><a href="?hl=th">ภาษาไทย</a><?php endif; ?>
                                     </div>
-                                    <div class="col-6 text-end small">
-                                        <i class="bi bi-chevron-left"></i> <a href="<?= getenv('main_site') ?>"><?= lang('System.go-to-main-site') ?></a>
-                                    </div>
                                 </div>
                                 <div class="row mt-3">
                                     <div class="col text-center small">
-                                        <?= lang('System.copyrights', [date('Y')]) ?>
+                                        <?= lang('System.copyrights', [date('Y')]) ?><br><a href="<?= getenv('main_site') ?>"><?= lang('System.go-to-main-site') ?></a>
                                     </div>
                                 </div>
                             </div>
