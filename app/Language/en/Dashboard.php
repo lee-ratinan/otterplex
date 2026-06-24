0defab4d19303bbfb72e533ba21031d5dcd4b01d
Ratinan Lee <lee.ratinan@gmail.com>
2026-06-24 23:20:44 +0800

    dashboard wording

--- app/Language/en/Dashboard.php
+++ app/Language/en/Dashboard.php
@@ -7,12 +7,12 @@ return [
         'variant_count'        => '{0} variant(s)',
         'check-business-setup' => 'Your business data is partially set during your sign up process. You may verify your business information and continue your setup here:',
         'branches-you-have'    => '<i class="fa-solid fa-circle-check text-success"></i> You currently have {0} branch(es).',
-        'branches-you-dont'    => '<i class="fa-solid fa-circle-xmark text-danger"></i> You have no branches setup. Please setup your first branch.',
+        'branches-you-dont'    => '<i class="fa-solid fa-circle-xmark text-danger"></i> You have no branches set up. Please set up your first branch.',
         'staff-you-have'       => '<i class="fa-solid fa-circle-check text-success"></i> You currently have {0} staff allocation(s). Please verify your staff allocation regularly to ensure accuracy.',
-        'staff-you-dont'       => '<i class="fa-solid fa-circle-xmark text-danger"></i> You have no staff allocations setup for the branches. Please setup your first staff member to the branches.',
+        'staff-you-dont'       => '<i class="fa-solid fa-circle-xmark text-danger"></i> You have no staff allocations setup for the branches. Please set up your first staff member to the branches.',
         'services-you-have'    => '<i class="fa-solid fa-circle-check text-success"></i> You currently have {0} service(s). Please verify your services regularly to ensure accuracy.',
-        'services-you-dont'    => '<i class="fa-solid fa-circle-xmark text-danger"></i> You have no services setup. Please setup your first service.',
+        'services-you-dont'    => '<i class="fa-solid fa-circle-xmark text-danger"></i> You have no services set up. Please set up your first service.',
         'products-you-have'    => '<i class="fa-solid fa-circle-check text-success"></i> You currently have {0} product(s). Please verify your products regularly to ensure accuracy.',
-        'products-you-dont'    => '<i class="fa-solid fa-circle-xmark text-danger"></i> You have no products setup. Please setup your first product.',
+        'products-you-dont'    => '<i class="fa-solid fa-circle-xmark text-danger"></i> You have no products set up. Please set up your first product.',
     ]
 ];
